5a07de5a66369c83eefdd6ef97cc2800ba86cc9e
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-05-28 14:27:57 -0400

    beginning format

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
 
 class App extends Component {
+  state = {
+    score: 0
+  }
+
+  updateScore = () => {}
+
+  subtractOneFromScore = () => {
+    console.log('button was clicked')
+    score -= 1
+    if (score < 0) {
+      this.setState({
+        score: score - 1
+      })
+    }
+  }
+
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <Header />
+        <main className="side-by-side">
+          <section className="team-1">
+            <h1>{this.state.score1}</h1>
+            <h2>Team 1</h2>
+            <button>Add One</button>
+            <button onClick={() => this.subtractOneFromScore()}>
+              Subtract One
+            </button>
+          </section>
+          <section className="team-2">
+            <h1 className="score-2">0</h1>
+            <h2>Team 2</h2>
+            <button>Add One</button>
+            <button>Subtract One</button>
+          </section>
+        </main>
+      </>
+    )
   }
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return <header className="header">Who Will Win?</header>
+  }
+}
+
+export default Header
