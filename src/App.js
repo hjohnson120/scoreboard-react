2b9fffadc75edfc4c704b4baf89e09716b813d39
Heather Johnson-Ledlie <heatherjohnson120@yahoo.com>
2019-05-28 15:45:17 -0400

    basic app js completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
 
+let score1 = 0
+let score2 = 0
+
 class App extends Component {
   state = {
-    score: 0
+    score1: 0,
+    score2: 0
   }
 
-  updateScore = () => {}
-
-  subtractOneFromScore = () => {
-    console.log('button was clicked')
-    score -= 1
-    if (score < 0) {
+  updateScore1 = newScore => {
+    if (newScore >= 0) {
       this.setState({
-        score: score - 1
+        score1: newScore
+      })
+    }
+  }
+
+  updateScore2 = newScore => {
+    if (newScore >= 0) {
+      this.setState({
+        score2: newScore
       })
     }
   }
@@ -26,16 +34,22 @@ class App extends Component {
           <section className="team-1">
             <h1>{this.state.score1}</h1>
             <h2>Team 1</h2>
-            <button>Add One</button>
-            <button onClick={() => this.subtractOneFromScore()}>
+            <button onClick={() => this.updateScore1(this.state.score1 + 1)}>
+              Add One
+            </button>
+            <button onClick={() => this.updateScore1(this.state.score1 - 1)}>
               Subtract One
             </button>
           </section>
           <section className="team-2">
-            <h1 className="score-2">0</h1>
+            <h1>{this.state.score2}</h1>
             <h2>Team 2</h2>
-            <button>Add One</button>
-            <button>Subtract One</button>
+            <button onClick={() => this.updateScore2(this.state.score2 + 1)}>
+              Add One
+            </button>
+            <button onClick={() => this.updateScore2(this.state.score2 - 1)}>
+              Subtract One
+            </button>
           </section>
         </main>
       </>
